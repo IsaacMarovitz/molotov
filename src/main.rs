d792bd453eb83f23b81bb54a6cc7247b13b815da
Isaac Marovitz <isaacryu@icloud.com>
2023-11-20 13:51:26 +0000

    LICENSE + Cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+use clap::Parser;
+
 mod dlls;
 mod loader;
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    exec: String
+}
+
 fn main() {
+    let args = Args::parse();
+
     println!("Hello, world!");
 }

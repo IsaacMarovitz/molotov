d750aab0c57983b0d9f6cd7c9844d8bf4eba2a56
Isaac Marovitz <isaacryu@icloud.com>
2023-11-20 17:42:38 +0000

    Values

--- src/dlls/ntdll/version.rs
+++ src/dlls/ntdll/version.rs
@@ -2,18 +2,32 @@ use crate::dlls::ntdll::ntstatus::NTSTATUS;
 use crate::dlls::ntdll::osversioninfo::{RTL_OSVERSIONINFOEXW};
 use std::mem;
 
+const CURRENT_VERSION: RTL_OSVERSIONINFOEXW = RTL_OSVERSIONINFOEXW {
+    dwOSVersionInfoSize: 0,
+    dwMajorVersion: 0,
+    dwMinorVersion: 0,
+    dwBuildNumber: 0,
+    dwPlatformId: 0,
+    szCSDVersion: [0; 128],
+    wServicePackMajor: 0,
+    wServicePackMinor: 0,
+    wSuiteMask: 0,
+    wProductType: 0,
+    wReserved: 0,
+};
+
 #[allow(non_snake_case)]
 pub fn RtlGetVersion(info: *mut RTL_OSVERSIONINFOEXW) -> NTSTATUS {
     unsafe {
         let info = &mut *info;
-        info.dwMajorVersion = 0;
-        info.dwMinorVersion = 0;
-        info.dwPlatformId = 0;
+        info.dwMajorVersion = CURRENT_VERSION.dwMajorVersion;
+        info.dwMinorVersion = CURRENT_VERSION.dwMinorVersion;
+        info.dwPlatformId = CURRENT_VERSION.dwPlatformId;
         if info.dwOSVersionInfoSize == mem::size_of::<RTL_OSVERSIONINFOEXW>() as u32 {
-            info.wServicePackMajor = 0;
-            info.wServicePackMinor = 0;
-            info.wSuiteMask = 0;
-            info.wProductType = 0;
+            info.wServicePackMajor = CURRENT_VERSION.wServicePackMajor;
+            info.wServicePackMinor = CURRENT_VERSION.wServicePackMinor;
+            info.wSuiteMask = CURRENT_VERSION.wSuiteMask;
+            info.wProductType = CURRENT_VERSION.wProductType;
         }
     }
 
